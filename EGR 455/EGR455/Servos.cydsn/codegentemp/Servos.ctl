ab178bad303ead8e186b3b018434f800e9e4f7dc
Jacob <jacob.knaup@asu.edu>
2017-08-28 20:25:28 -0700

    1st 455 servo project

---
+++ EGR 455/EGR455/Servos.cydsn/codegentemp/Servos.ctl
@@ -0,0 +1,9 @@
+-- ======================================================================
+-- Servos.ctl generated from Servos
+-- 08/28/2017 at 11:20
+-- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
+-- ======================================================================
+
+-- PSoC Clock Editor
+-- Directives Editor
+-- Analog Device Editor
--- EGR 455/EGR455/Servos.cydsn/main.c
+++ EGR 455/EGR455/Servos.cydsn/main.c
@@ -11,15 +11,36 @@
 */
 #include "project.h"
 
+int theta1(float angle){
+    int max_comp = 6450, min_comp = 1600, max_angle=180, min_angle=0;
+    int compare;
+    compare = (max_comp-min_comp)/(max_angle-min_angle)*(angle-min_angle) + min_comp;
+    return compare;
+}
+int theta2(float angle){
+    int max_comp = 7000, min_comp = 2100, max_angle=90, min_angle=-90;
+    int compare;
+    compare = (max_comp-min_comp)/(max_angle-min_angle)*(angle-min_angle) + min_comp;
+    return compare;
+}
+
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    pwm1_Start();
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for(;;)
     {
-        /* Place your application code here. */
+        pwm1_WriteCompare1(theta1(0)); //servo 1 full clockwise
+        CyDelay(2000);
+        pwm1_WriteCompare1(theta1(180)); //servo 1 full ccw
+        CyDelay(2000);
+      
+        pwm1_WriteCompare2(theta2(-90)); //servo 2 full clockwise
+        CyDelay(2000);
+        pwm1_WriteCompare2(theta2(90)); //servo 2 full ccw
+        CyDelay(2000);
     }
 }
 
