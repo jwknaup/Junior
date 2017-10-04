4fae452b34b874773b4bf2449c134abe6261c8b7
Jacob <jacob.knaup@asu.edu>
2017-10-03 21:03:15 -0700

    finish tech paper, research paper notes, exam 2 ppt

--- IDEAlab/LoadCellDataAcquisition.m
+++ IDEAlab/LoadCellDataAcquisition.m
@@ -2,9 +2,11 @@ delete(instrfindall);
 s = serial('COM12');
 set(s, 'BaudRate', 115200);
 fopen(s);
-fprintf(s, '?');
-fprintf(s, char(13));
-out = fscanf(s)
+while(cputime<1000)
+    fprintf(s, '?');
+    fprintf(s, char(13));
+    out = fscanf(s)
+end
 fclose(s);
 delete(s);
 clear s
