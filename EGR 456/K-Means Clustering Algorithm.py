6b6221afd7d3dec7f59590df04317b6726806b6a
Jacob <jacob.knaup@asu.edu>
2018-02-20 17:58:15 -0700

    neural net

--- EGR 456/K-Means Clustering Algorithm.py
+++ EGR 456/K-Means Clustering Algorithm.py
@@ -7,7 +7,7 @@ Created on Thu Feb 15 13:18:56 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
 ax=fig.add_subplot(111,projection='3d')
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/jacob/Junior/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/jacob/Junior/catkin_ws/devel;/home/jacob/catkinws/devel;/opt/ros/kinetic".split(';') if "/home/jacob/Junior/catkin_ws/devel;/home/jacob/catkinws/devel;/opt/ros/kinetic" != "" else []
