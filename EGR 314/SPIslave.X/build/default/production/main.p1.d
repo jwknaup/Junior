490a0b45c868695d20e90c9142db5f7beaa94d5d
Jacob <jacob.knaup@asu.edu>
2018-04-18 12:04:03 -0700

    meet with Aukes, spi slave

---
+++ EGR 314/SPIslave.X/build/default/production/main.p1.d
@@ -0,0 +1,7 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/spi2_types.h  \
+mcc_generated_files/spi2_driver.h  \
+mcc_generated_files/pin_manager.h 
---
+++ EGR 314/SPIslave.X/build/default/production/mcc_generated_files/spi2_driver.p1.d
@@ -0,0 +1,7 @@
+ build/default/production/mcc_generated_files/spi2_driver.d  \
+ build/default/production/mcc_generated_files/spi2_driver.p1:  \
+ mcc_generated_files/spi2_driver.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/spi2_types.h  \
+mcc_generated_files/spi2_driver.h  \
+mcc_generated_files/pin_manager.h 
